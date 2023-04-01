c5389318919922c582f1a0d049e7dddb830b2a42
luki <letitbegos@gmail.com>
2023-04-01 15:18:19 +0800

    database name changed

--- src/db.rs
+++ src/db.rs
@@ -6,7 +6,7 @@ use wither::mongodb;
 lazy_static! {
     pub static ref CONNECTION: AsyncOnce<Database> = AsyncOnce::new(async {
       let db_uri = "mongodb://localhost:27017";
-      let db_name = "r_blog";
+      let db_name = "unshy_backend";
       mongodb::Client::with_uri_str(db_uri)
         .await
         .expect("Failed to initialize MongoDB connection")
