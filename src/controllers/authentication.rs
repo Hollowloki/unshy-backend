e04a3169926e57ee05d59cf0f18b81a2682e607b
luki <letitbegos@gmail.com>
2023-04-01 15:19:01 +0800

    user register method added

--- src/controllers/authentication.rs
+++ src/controllers/authentication.rs
@@ -1,28 +1,24 @@
+use std::time::Instant;
+
 use axum::{http::{StatusCode}, Json};
 use serde::{Deserialize, Serialize};
 use serde_json::{Value, json};
 
+use crate::{errors::Error, utils::{custom_response::{CustomResponse, CustomResponseBuilder}, models::ModelExt}, models::user_model::{User, hash_password}};
+
 #[derive(Deserialize, Serialize, Debug)]
-pub struct Login {
-    username_or_email: String,
-    password: String,
-}
-
-
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct User {
+pub struct Register {
     username: String,
     email: String,
     password: String,
 }
-#[derive(Deserialize, Serialize, Debug)]
-pub struct Claims {
-    user: User,
-    exp: usize,
-}
 
 
 
-pub async fn register(_credentials: Option<Json<User>>) -> Result<Json<Value>, StatusCode> {
-    return Ok(Json(json!({"message": "Hello World"})));
+pub async fn register(Json(credentials): Json<Register>) -> Result<CustomResponse<User>, Error> {
+    let password_hash = hash_password(credentials.password).await?;
+    let user = User::new(credentials.username, credentials.email, password_hash);
+    let res = User::create(user).await?;
+    let res = CustomResponseBuilder::new().body(res).status(StatusCode::CREATED).build();
+    Ok(res)
 }
