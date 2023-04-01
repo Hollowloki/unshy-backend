63cbffc011799d4d85b0a85f395815c53221a55b
luki <letitbegos@gmail.com>
2023-04-01 18:00:53 +0800

    route fixes

--- src/router.rs
+++ src/router.rs
@@ -1,13 +1,14 @@
 pub mod routes {
     use axum::{Router, routing::{post}, };
 
-    use crate::controllers::{article::write_post, authentication::register};
+    use crate::controllers::{article::write_post, authentication::{register, authenticate_user}};
     
         pub async fn routes() -> Router
         {
             let authenticaiton = Router::new()
-            .route("/register", post(write_post))
-            .route("/hello", post(register));
+            .route("/article", post(write_post))
+            .route("/register", post(register))
+            .route("/login", post(authenticate_user));
             return authenticaiton;
         }
 }
