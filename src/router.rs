e447453b9539524688b0238a7f597c59285f9a71
luki <letitbegos@gmail.com>
2023-04-07 18:49:57 +0800

    book router added

--- src/router.rs
+++ src/router.rs
@@ -1,14 +1,19 @@
 pub mod routes {
     use axum::{Router, routing::{post}, };
+    use http::header;
+    use tower_http::sensitive_headers::SetSensitiveHeadersLayer;
 
-    use crate::controllers::{article::write_post, authentication::{register, authenticate_user}};
+    use crate::controllers::{authentication::{register, authenticate_user}, books::create_book};
     
         pub async fn routes() -> Router
         {
             let authenticaiton = Router::new()
-            .route("/article", post(write_post))
             .route("/register", post(register))
-            .route("/login", post(authenticate_user));
+            .route("/login", post(authenticate_user))
+            .route("/books", post(create_book))
+            .layer(SetSensitiveHeadersLayer::new(std::iter::once(
+                header::AUTHORIZATION,
+              )));
             return authenticaiton;
         }
 }
