57a134c282f8de37c6b016d755e3afe9feee772a
luki <letitbegos@gmail.com>
2023-04-07 18:47:23 +0800

    public user created

--- src/models/user_model.rs
+++ src/models/user_model.rs
@@ -1,3 +1,4 @@
+use bson::serde_helpers::{serialize_object_id_as_hex_string, bson_datetime_as_rfc3339_string};
 use serde::{Serialize, Deserialize};
 use tokio::task;
 use validator::Validate;
@@ -53,6 +54,29 @@ impl User {
     }
   }
 
+  #[derive(Debug, Serialize, Deserialize)]
+  pub struct PublicUser {
+    #[serde(alias = "_id", serialize_with = "serialize_object_id_as_hex_string")]
+    pub id: ObjectId,
+    pub name: String,
+    pub email: String,
+    #[serde(with = "bson_datetime_as_rfc3339_string")]
+    pub updated_at: Date,
+    #[serde(with = "bson_datetime_as_rfc3339_string")]
+    pub created_at: Date,
+  }
+  
+  impl From<User> for PublicUser {
+    fn from(user: User) -> Self {
+      Self {
+        id: user.id.unwrap(),
+        name: user.name.clone(),
+        email: user.email.clone(),
+        updated_at: user.updated_at,
+        created_at: user.created_at,
+      }
+    }
+  }
 
   pub async fn hash_password<P>(password: P) -> Result<String, Error>
 where
