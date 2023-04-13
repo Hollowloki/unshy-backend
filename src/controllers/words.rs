ee0fa7adbbc8f8778211fc4d42d43ad7093227bb
luki <letitbegos@gmail.com>
2023-04-13 15:49:07 +0800

    fixed logic

--- src/controllers/words.rs
+++ src/controllers/words.rs
@@ -1,20 +1,34 @@
 use axum::extract::Json;
 use bson::oid::ObjectId;
+use http::StatusCode;
+use serde::{Deserialize, Serialize};
 
 use crate::{
     errors::Error,
     models::word_model::Word,
-    utils::{models::ModelExt, token::TokenUser},
+    utils::{
+        custom_response::{CustomResponse, CustomResponseBuilder},
+        models::ModelExt,
+        token::TokenUser,
+    },
 };
 
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct WordRequest {
     word: String,
     definition: String,
 }
 
-pub async fn save_word(user: TokenUser, Json(context): Json<WordRequest>) -> Result<Word, Error> {
+pub async fn save_word(
+    user: TokenUser,
+    Json(context): Json<WordRequest>,
+) -> Result<CustomResponse<Word>, Error> {
     let user_id = user.id;
     let word = Word::new(context.word, context.definition, ObjectId::new(), user_id);
     let word = Word::create(word).await?;
-    Ok(word)
+    let res = CustomResponseBuilder::new()
+        .body(word)
+        .status(StatusCode::CREATED)
+        .build();
+    Ok(res)
 }
--- src/router.rs
+++ src/router.rs
@@ -1,17 +1,22 @@
 pub mod routes {
-    use axum::{routing::post, Router};
+    use axum::{
+        routing::{get, post},
+        Router,
+    };
     use http::header;
     use tower_http::sensitive_headers::SetSensitiveHeadersLayer;
 
     use crate::controllers::{
         authentication::{authenticate_user, register},
         books::create_book,
+        words::save_word,
     };
 
     pub async fn routes() -> Router {
         let authenticaiton = Router::new()
             .route("/register", post(register))
             .route("/login", post(authenticate_user))
+            .route("/words", post(save_word))
             .route("/books", post(create_book))
             .layer(SetSensitiveHeadersLayer::new(std::iter::once(
                 header::AUTHORIZATION,
